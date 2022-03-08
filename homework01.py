67b44247d09f2824cd4ac535c9e00a0d9b4e1efb
PanLoyso <85990809+PanLoyso@users.noreply.github.com>
2022-03-08 21:34:11 +0300

    Delete homework01.py

--- homework01.py
+++
@@ -1,9 +0,0 @@
-a = int(5)
-b = float(2.4)
-c = str("Привет")
-d = [20, 30, 40]
-e = {"haracter": "elf", "profession": "archer"}
-f = ("John", 10)
-list = [a, b, c, d, e, f]
-for i in list:
-    print(f"{i} is {type(i)}")
