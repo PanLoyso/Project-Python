c5423be89fca8f0356c4e4a089cafc5764594cda
PanLoyso <85990809+PanLoyso@users.noreply.github.com>
2022-03-08 21:34:19 +0300

    Delete homework02.py

--- homework02.py
+++
@@ -1,7 +0,0 @@
-a = int(input("Введите значение >>> "))
-b = int(input("Введите значение >>> "))
-c = int(input("Введите значение >>> "))
-d = int(input("Введите значение >>> "))
-numbers = [a, b, c, d]
-numbers[::2], numbers[1::2] = numbers[1::2], numbers[::2]
-print(numbers)
