9336d1a3be297fea6e16ff4cd32aed71fbfe1549
PanLoyso <85990809+PanLoyso@users.noreply.github.com>
2022-03-14 23:49:01 +0300

    Delete homework01.py

--- homework01.py
+++
@@ -1,9 +0,0 @@
-def calculate(a, b):
-    try:
-        return a / b
-    except ZeroDivisionError:
-        return ("Деление на ноль запрещено")
-print(calculate(
-    int(input("Введите число а >>> ")),
-    int(input("Введите число b >>> "))
-))
